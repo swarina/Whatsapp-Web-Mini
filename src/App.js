cdd6a3924cbe36d9ad2a6b85fe0fd31191f7d8b0
swarina <swarinajaiswal@gmail.com>
2021-08-04 21:02:56 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
