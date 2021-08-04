ed70f3ae2d8721aa85f2dc73d9d28da4db0694d7
swarina <swarinajaiswal@gmail.com>
2021-08-04 20:59:41 +0530

    Add Firebase Config object

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBL70y-SMijTyOvMAiXpbRgLwYPrJmsvtA",
+  authDomain: "whatsapp-web-clone-24be9.firebaseapp.com",
+  projectId: "whatsapp-web-clone-24be9",
+  storageBucket: "whatsapp-web-clone-24be9.appspot.com",
+  messagingSenderId: "199990225392",
+  appId: "1:199990225392:web:8967747d78182cb718d535",
+  measurementId: "G-YRYLC9057J",
+};
