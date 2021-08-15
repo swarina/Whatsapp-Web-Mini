9599d862290f745f922066fbef3e2df7d0c3aa42
swarina <swarinajaiswal@gmail.com>
2021-08-15 23:32:35 +0530

    Enable google authentication for signup

--- src/Login.js
+++ src/Login.js
@@ -6,8 +6,16 @@ import "./Login.css";
 //Material-UI
 import { Button } from "@material-ui/core";
 
+//Firebase
+import { auth, provider } from "./firebase";
+
 function Login() {
-  const signIn = () => {};
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => console.log(result))
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="login">
